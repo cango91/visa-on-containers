0bedb5022d2906ba4fb1bf6c7ff7174ca8e0ffc4
cango91 <can.gologlu@gmail.com>
2023-09-29 12:17:11 -0400

    Bare bones service orchestration

---
+++ services/authentication/src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => res.send('Hello, TypeScript!'));
+
+app.listen(port, () => console.log(`Authentication service running at http://localhost:${port}/`));
