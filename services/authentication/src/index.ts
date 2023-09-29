8a18d9ce6220e01a84f36bfa2bd34fdcb36efb11
cango91 <can.gologlu@gmail.com>
2023-09-29 14:48:42 -0400

    Multiple secrets management

--- services/authentication/src/index.ts
+++ services/authentication/src/index.ts
@@ -4,6 +4,6 @@ import express from 'express';
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello, TypeScript!'));
+app.get('/', (req, res) => res.send('Hello, TypeScript! This is auth service.'));
 
 app.listen(port, () => console.log(`Authentication service running at http://localhost:${port}/`));
